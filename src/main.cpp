7a76ce15ee81d0595c8d3366a78eec0d0d88c093
VRxiaojie <1814014516@qq.com>
2025-07-25 10:32:24 +0800

    add: OLED显示激活提示

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,14 @@ void setup()
   // 如果未激活
   if (EEPROM_read_activation() == false)
   {
+    u8g2.setFont(u8g2_font_wqy12_t_gb2312a);
+    u8g2.drawUTF8(0, 16, "---设备未激活!---");
+    u8g2.drawUTF8(0, 32, "请用PC端激活软件激活,");
+    u8g2.drawUTF8(0, 48, "每人免费激活一台设备.");
+    u8g2.drawUTF8(0, 64, "如需激活多台设备,");
+    u8g2.drawUTF8(0, 80, "请私信作者");
+    u8g2.drawUTF8(0, 96, "遇问题请在工程下留言");
+    u8g2.sendBuffer();
     activation_loop();
   }
 
