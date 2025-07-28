727974d9089b65f4aa9b3ad3846327c389ac401c
VRxiaojie <1814014516@qq.com>
2025-07-28 17:58:11 +0800

    fix: 电子开关引脚和INA226的顺序颠倒的问题

--- src/ina226_data.cpp
+++ src/ina226_data.cpp
@@ -3,7 +3,7 @@
 INA226_DATA_t ina226_data[4];
 
 INA226 ina226_ctrl[4] = {
-    INA226(0x40),
-    INA226(0x41),
+    INA226(0x45),
     INA226(0x44),
-    INA226(0x45)};
+    INA226(0x41),
+    INA226(0x40)};
