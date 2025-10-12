88c6e4e3e59b75c417cc343cfcace100579203b0
VRxiaojie <1814014516@qq.com>
2025-10-12 20:46:12 +0800

    docs: websocket相关注释

--- include/ws.h
+++ include/ws.h
@@ -5,5 +5,13 @@
 #include "switch.h"
 extern WebSocketsServer webSocket;
 
+/**
+ * @brief WebSocket事件处理函数
+ *
+ * @param num 客户端编号
+ * @param type 事件类型 (e.g., WStype_DISCONNECTED, WStype_CONNECTED, WStype_TEXT)
+ * @param payload 接收到的数据负载
+ * @param length 数据负载的长度
+ */
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length);
 #endif
