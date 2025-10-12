ad8922d0508e80396ec7e7558e45d76d24b677df
VRxiaojie <1814014516@qq.com>
2025-10-12 21:00:38 +0800

    docs: OTA

--- include/OTA.h
+++ include/OTA.h
@@ -1,6 +1,15 @@
 #ifndef OTA_H_
 #define OTA_H_
 #define FIRMWARE_VERSION "1.2.2"
+/**
+ * @brief 检查是否有可用的OTA更新
+ * @return true 如果有可用的更新
+ * @return false 如果没有可用的更新
+ */
 bool checkForOTA();
+
+/**
+ * @brief 如果OTA更新准备就绪，则执行更新
+ */
 void checkOTAReady();
 #endif
