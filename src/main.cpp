215c997fbacf66228b19102e9a513c800a82e958
VRxiaojie <1814014516@qq.com>
2025-10-12 20:16:08 +0800

    remove: 激活功能

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@
 #include "switch.h"
 #include "ina226_data.h"
 #include "Wifi_Config.h"
-#include "encrypt.h"
 #include "OTA.h"
 
 void setup()
@@ -19,27 +18,6 @@ void setup()
   Wire.begin(SDA, SCL);
   oled_init();
 
-  encrypt_mac();
-  // 如果未激活
-  if (EEPROM_read_activation() == false)
-  {
-    uint8_t mac[6];
-    char mac_str[13];
-    esp_efuse_mac_get_default(mac);
-    sprintf(mac_str, "%02X%02X%02X%02X%02X%02X",
-            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    u8g2.setFont(u8g2_font_wqy12_t_gb2312a);
-    u8g2.drawUTF8(0, 16, "-----设备未激活-----");
-    u8g2.drawUTF8(0, 32, "请用PC端激活软件激活,");
-    u8g2.drawUTF8(0, 48, "每人免费激活一台设备.");
-    u8g2.drawUTF8(0, 64, "激活多台设备的方法,");
-    u8g2.drawUTF8(0, 80, "见激活工具的底部.");
-    u8g2.drawUTF8(0, 96, "遇问题请在工程下留言");
-    u8g2.setCursor(0, 112);
-    u8g2.printf("MAC: %s", mac_str);
-    u8g2.sendBuffer();
-    activation_loop();
-  }
   // 检查是否进入OTA更新模式
   checkOTAReady();
 
